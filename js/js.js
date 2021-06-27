edf930ce24a864ed666c1ae38d90ab4fa65d4b9f
username-nickname <86109747+username-nickname@users.noreply.github.com>
2021-06-27 23:18:42 +0300

    Rou

---
+++ js/js.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.slider').slick({
+  arrows: false,
+  infinite: true,
+  dots: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  });
+    
+});
